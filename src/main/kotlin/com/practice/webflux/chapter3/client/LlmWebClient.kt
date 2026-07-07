65aaa2a7620c3a5987953b46c24b2135a09b4e60
kkangh00n <kig2454@gmail.com>
2026-07-07 17:29:38 +0900

    예외로 인해, 스트림을 중단하고 싶지 않을 경우

--- src/main/kotlin/com/practice/webflux/chapter3/client/LlmWebClient.kt
+++ src/main/kotlin/com/practice/webflux/chapter3/client/LlmWebClient.kt
@@ -2,12 +2,32 @@ package com.practice.webflux.chapter3.client
 
 import com.practice.webflux.chapter3.client.dto.LlmChatRequestDto
 import com.practice.webflux.chapter3.client.dto.LlmChatResponseDto
+import com.practice.webflux.chapter3.exception.CommonError
+import com.practice.webflux.chapter3.exception.ErrorTypeException
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import java.util.function.Function
 
 interface LlmWebClient {
 
     fun getChatCompletion(requestDto: LlmChatRequestDto): Mono<LlmChatResponseDto>
+
+    /**
+     * 스트림 내 발생한 예외로 인해, 스트림을 중단하고 싶지 않을 경우 사용!
+     */
+    fun getChatCompletionWithCatchException(requestDto: LlmChatRequestDto?): Mono<LlmChatResponseDto> {
+        return getChatCompletion(requestDto!!)
+            .onErrorResume(Function { exception: Throwable? ->
+                if (exception is ErrorTypeException) {
+                    val commonError =
+                        CommonError(exception.errorType.code, exception.message)
+                    Mono.just(LlmChatResponseDto(commonError))
+                } else {
+                    val commonError = CommonError(500, exception!!.message)
+                    Mono.just(LlmChatResponseDto(commonError))
+                }
+            })
+    }
 
     fun getLlmType(): LlmType
 
--- src/main/kotlin/com/practice/webflux/chapter3/client/dto/LlmChatResponseDto.kt
+++ src/main/kotlin/com/practice/webflux/chapter3/client/dto/LlmChatResponseDto.kt
@@ -1,15 +1,25 @@
 package com.practice.webflux.chapter3.client.dto
 
 import com.practice.webflux.chapter3.client.gemini.dto.GeminiChatResponseDto
+import com.practice.webflux.chapter3.exception.CommonError
 import java.io.Serializable
 
 data class LlmChatResponseDto(
-    var llmResponse: String
+    var llmResponse: String? = null,
+    var error: CommonError? = null
+
 ) : Serializable {
 
     constructor(
         geminiChatResponseDto: GeminiChatResponseDto
     ) : this(
         llmResponse = geminiChatResponseDto.getSingleText()
+    )
+
+    constructor(
+        error: CommonError
+    ) : this(
+        llmResponse = null,
+        error = error
     )
 }
